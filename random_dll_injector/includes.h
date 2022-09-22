3a9872138ccedbbfc8ea0d840bae617f30591f84
dev-randomYT <adrian@zivkovic.com.au>
2022-09-22 10:29:57 +1000

    V1.0.1

--- random_dll_injector/includes.h
+++ random_dll_injector/includes.h
@@ -2,3 +2,7 @@
 #include <Windows.h>
 #include <iostream>
 #include <TlHelp32.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
--- random_dll_injector/main.cpp
+++ random_dll_injector/main.cpp
@@ -1,6 +1,8 @@
 #include "includes.h"
 #include "functions.h"
 
+std::string DLL_NAME;
+
 void Cleanup(const std::string message) {
 	std::cout << message << std::endl;
 	system("pause");
@@ -17,6 +19,15 @@ std::string RandomString(const size_t length) {
 }
 
 int main() {
+
+	std::cout << "WELCOME TO RANDOM_VAC_INJECTOR \n";
+	std::cout << "\n";
+
+	std::cout << "What is the name of the dll to be injected | ";
+	std::cin >> DLL_NAME;
+
+	std::cout << "Selected DLL is | " << DLL_NAME << std::endl;
+	
 	SetConsoleTitleA(RandomString(26).c_str());
 
 	DWORD ProcessId = Functions::GetProcessId("csgo.exe");
@@ -29,12 +40,12 @@ int main() {
 
 	std::cout << "csgo.exe --> " << std::dec << ProcessId << std::endl;
 
-	if (Functions::DoesFileExist("hack.dll")) {
+	if (Functions::DoesFileExist(DLL_NAME.c_str())) {
 		if (!Functions::Internal::ExecuteBypass(game)) {
 			Cleanup("Cannot Bypass ");
 		}
 
-		if (Functions::LoadLibraryInject(ProcessId, "hack.dll")) {
+		if (Functions::LoadLibraryInject(ProcessId, DLL_NAME.c_str())) {
 			Functions::Internal::Backup(game);
 			std::cout << "Injected" << std::endl;
 			system("pause");
